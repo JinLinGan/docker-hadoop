94ede9aea674ed5af0d4e5c51ccb79553e6e89e1
JinLin Gan <JinLin.Gan@Gmail.com>
2019-12-09 20:36:42 +0800

    update

--- bin/app.sh
+++ bin/app.sh
@@ -12,18 +12,21 @@ fi
 echo -e "开始生成测试文件，约 500MB"
 for x in {1..5000}
 do 
-    cat /tmp/in.txt >> ./test.file
+    cat /tmp/in.txt >> /tmp/test.file
 done
 echo -e "生成结束"
-echo -e "开始提交测试文件到 HDFS /tmp/test.file"
-hadoop fs -moveFromLocal ./test.file /tmp/
-echo -e "提交结束 HDFS /tmp/test.file"
+
 
 echo -e "开始循环运行测试案例"
 while :
 do
+echo -e "开始提交测试文件到 HDFS /tmp/test.file"
+hadoop fs -moveFromLocal /tmp/test.file /tmp/
+echo -e "提交结束 HDFS /tmp/test.file"
+
 hadoop fs -ls /tmp
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar wordcount   /tmp/test.file /tmp/out
 hadoop fs -cat /tmp/out/part-r-00000
 hadoop fs -rm -r -f /tmp/out
+hadoop fs -rm -r -f /tmp/test.file
 done
