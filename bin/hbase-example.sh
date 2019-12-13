dca4670831f0c0b798001589fb10f86c0de4887a
JinLin Gan <JinLin.Gan@Gmail.com>
2019-12-13 18:04:35 +0800

    update

---
+++ bin/hbase-example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./wait-for-it.sh hbase-master:60010 -t 600
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "\n--------------------------------------------"
+    echo -e "       YARN Application History not ready! ..."
+    echo -e "--------------------------------------------"
+    exit $rc
+fi
+HBASE_EXAMPLE_CLASSPATH=`./bin/hbase classpath`
+
+count=1
+echo -e "开始循环运行测试案例"
+while :
+do
+echo -e "开始第 $count 次任务"
+java -cp hbase-examples-1.2.0-cdh5.16.2.jar:$HBASE_EXAMPLE_CLASSPATH  org.apache.hadoop.hbase.thrift.DemoClient hbase-master 9090
+echo -e "第 $count 次任务结束"
+count=$(( count+1 ))
+done
