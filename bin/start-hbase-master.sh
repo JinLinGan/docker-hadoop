a06cae22dfade47e891fda1acbe9c69e4b291cd0
JinLin Gan <JinLin.Gan@Gmail.com>
2019-12-13 14:08:09 +0800

    update

--- bin/start-hbase-master.sh
+++ bin/start-hbase-master.sh
@@ -2,6 +2,8 @@
 
 echo -e "\n---------------------------------------"
 
+sudo -E -u hdfs hdfs dfs -mkdir /hbase
+sudo -E -u hdfs hdfs dfs -chown hbase /hbase
 
 echo -e	"Starting hbase-master..."
 supervisorctl start hbase-master
